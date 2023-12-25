c49ccd15da5792961cb362ef2a393fe6f637aff3
LVJameskirk <LVJameskirk@gmail.com>
2023-12-25 13:20:40 +0300

    feat: add solutions for carbon-dating and common-character-count

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let commonCount = 0;
+  const s1CharCount = {};
+  const s2CharCount = {};
+
+  for (let char of s1) {
+    if (s1CharCount[char]) {
+      s1CharCount[char]++;
+    } else {
+      s1CharCount[char] = 1;
+    }
+  }
+
+  for (let char of s2) {
+    if (s2CharCount[char]) {
+      s2CharCount[char]++;
+    } else {
+      s2CharCount[char] = 1;
+    }
+  }
+
+  for (let char in s1CharCount) {
+    if (s2CharCount[char]) {
+      commonCount += Math.min(s1CharCount[char], s2CharCount[char]);
+    }
+  }
+
+  return commonCount;
 }
 
 module.exports = {
