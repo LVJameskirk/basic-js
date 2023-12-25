be89049190f9300ddc124827db8a8890f9cee529
LVJameskirk <LVJameskirk@gmail.com>
2023-12-25 13:39:55 +0300

    feat: add solutions hanoi-tower and transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const resultArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      i++;
+    } else if (arr[i] === '--discard-prev') {
+      if (i > 0 && arr[i - 2] !== '--discard-next') {
+        resultArr.pop();
+      }
+    } else if (arr[i] === '--double-next') {
+      if (i < arr.length - 1) {
+        resultArr.push(arr[i + 1]);
+      }
+    } else if (arr[i] === '--double-prev') {
+      if (i > 0 && arr[i - 2] !== '--discard-next') {
+        resultArr.push(arr[i - 1]);
+      }
+    } else {
+      resultArr.push(arr[i]);
+    }
+  }
+  
+  return resultArr;
 }
 
 module.exports = {
